7ae175877d47ce4294e66d7e52c52611387fc367
shelffim <shelffim0219@gmail.com>
2025-09-25 16:48:17 +0900

    java-adv1 section4 - 7 improve

--- assignment/src/adv1/assignment2/com/inventory/domain/Inventory.java
+++ assignment/src/adv1/assignment2/com/inventory/domain/Inventory.java
@@ -1,20 +1,28 @@
 package adv1.assignment2.com.inventory.domain;
 
 public class Inventory {
-    private volatile int stock;
+    private int stock;
 
     public Inventory(int stock) {
         this.stock = stock;
     }
 
-    public synchronized void restock(int quantity) {
-        this.stock += quantity;
-        System.out.println("[" + Thread.currentThread().getName() + "] >> 상품 " + quantity + "개 입고. [현재 재고: " + this.stock + "]");
+    public void restock(int quantity) {
+        int currentStock;
+        synchronized (this) {
+            this.stock += quantity;
+            currentStock = this.stock;
+        }
+        System.out.println("[" + Thread.currentThread().getName() + "] >> 상품 " + quantity + "개 입고. [현재 재고: " + currentStock + "]");
     }
 
-    public synchronized void ship(int quantity) {
-        this.stock -= quantity;
-        System.out.println("[" + Thread.currentThread().getName() + "] >> 상품 " + quantity + "개 판매. [현재 재고: " + this.stock + "]");
+    public void ship(int quantity) {
+        int currentStock;
+        synchronized (this) {
+            this.stock -= quantity;
+            currentStock = this.stock;
+        }
+        System.out.println("[" + Thread.currentThread().getName() + "] >> 상품 " + quantity + "개 판매. [현재 재고: " + currentStock + "]");
     }
 
     public synchronized int checkStock() {
