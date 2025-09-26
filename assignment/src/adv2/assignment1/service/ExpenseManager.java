b790e0f7ad6258a3b09234324e0920088532d19f
shelffim <shelffim0219@gmail.com>
2025-09-26 10:36:37 +0900

    java-adv2 section2 - 4 improve

--- assignment/src/adv2/assignment1/service/ExpenseManager.java
+++ assignment/src/adv2/assignment1/service/ExpenseManager.java
@@ -3,15 +3,12 @@ package adv2.assignment1.service;
 import adv2.assignment1.domain.Expense;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
 public class ExpenseManager {
-
-    private final List<Expense> expenseList = new ArrayList<>();
 
     public void saveExpenses(List<Expense> expenses, String fileName) throws IOException {
         FileOutputStream fos = new FileOutputStream(fileName);
@@ -41,34 +38,32 @@ public class ExpenseManager {
         BufferedInputStream bis = new BufferedInputStream(fis);
         DataInputStream dis = new DataInputStream(bis);
 
-        while (true) {
-            try {
+        try {
+            while (true) {
                 String date = dis.readUTF();
                 String item = dis.readUTF();
                 int price = dis.readInt();
                 String memo = dis.readUTF();
 
                 expenses.add(new Expense(date, item, price, memo));
-            } catch (IOException e) {
-                System.out.println("파일의 끝에 도착했습니다.");
-                break;
             }
+        }catch (EOFException e) {
+
         }
         dis.close();
         return expenses;
     }
 
     public void generateReport(List<Expense> expenses, String reportFileName) throws IOException {
-        FileWriter fw = new FileWriter(reportFileName, UTF_8);
-        fw.write("========================================\n");
-        fw.write("       지출 내역 보고서\n");
-        fw.write("========================================\n");
-        fw.write("날짜        | 항목          | 금액      | 메모\n");
-        fw.write("----------------------------------------\n");
+        PrintWriter pw = new PrintWriter(new FileWriter(reportFileName, UTF_8));
+        pw.println("========================================");
+        pw.println("       지출 내역 보고서");
+        pw.println("========================================");
+        pw.println("날짜        | 항목          | 금액      | 메모");
+        pw.println("----------------------------------------");
         for (Expense expense : expenses) {
-            String formattedString = String.format("%s  | %s     |   %d원 | %s\n", expense.getDate(), expense.getItem(), expense.getPrice(), expense.getMemo());
-            fw.write(formattedString);
+            pw.printf("%s  | %s     |   %d원 | %s\n", expense.getDate(), expense.getItem(), expense.getPrice(), expense.getMemo());
         }
-        fw.close();
+        pw.close();
     }
 }
